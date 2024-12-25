db0ecf584ed95a8e0cd96a48f82e5b5b907c1e6d
Starry03 <andrea.starrantino@gmail.com>
2024-12-25 13:22:12 +0100

    add initial section for Linux Events in README

--- main.c
+++ main.c
@@ -7,8 +7,10 @@
 int	main(void)
 {
 	t_event_driver	driver;
+	t_linkedlist	*buckets;
 	t_linkedlist	bucket;
-	t_dict_obj		obj;
+	t_dict_obj		dict_obj;
+	struct input_event		event;
 	t_dict			dict;
 
 	driver = event_driver_init("/dev/input/");
@@ -21,14 +23,24 @@ int	main(void)
 	printf("generated %ld threads\n", driver->thread_count);
 	if (!driver->thread_count)
 		return (EXIT_FAILURE);
-	// print events scanning 1st event in every dict bucket
 	dict = driver->event_multi_queue->data;
+	buckets = dict_get_buckets(dict);
 	while (1)
 	{
 		shared_rsc_wait(driver->event_multi_queue);
-		bucket = dict_get_buckets(dict);
-		shared_rsc_post(driver->event_multi_queue);
+		for (size_t i = 0; i < dict->size; i++)
+		{
+			bucket = buckets[i];
+			while (bucket)
+			{
+				dict_obj = (t_dict_obj)(linkedlist_getinfo(bucket));
+				event = *(struct input_event *)dict_obj->value;
+				printf("event[%zu]: %zu -> %d\n", i, *(size_t *)dict_obj->key, event.code);
+				bucket = linkedlist_getnext(bucket);
+			}
+		}
 		dict_status(dict);
+		shared_rsc_post(driver->event_multi_queue);
 		sleep(1);
 	}
 	event_driver_free(driver);
